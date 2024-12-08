cceef5308aeb6ab41d7c4f24abe205c65f752b3f
TonyLin <chiacheng.lin0@gmail.com>
2024-12-08 19:37:02 +0800

    feat: add Redis service to Docker configuration and update Redis client host

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "test") {
     console.log("Using Redis mock client");
 } else {
     redisClient = new Redis({
-        host: "127.0.0.1", // Redis 伺服器地址
+        host: "redis", // Redis 伺服器地址
         port: 6379,        // Redis 預設埠
         //password: process.env.REDIS_PASSWORD || undefined, // 如果需要密碼驗證
     });
