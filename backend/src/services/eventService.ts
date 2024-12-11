9a47555415947db4d917ae785800d8616f4d47c6
TonyLin <chiacheng.lin0@gmail.com>
2024-12-11 13:20:40 +0000

    feat: integrate Redis caching for event notifications and format notification data

--- backend/src/services/eventService.ts
+++ backend/src/services/eventService.ts
@@ -1,8 +1,15 @@
 // services/eventService.ts
 import { Event, IEventDocument } from '@src/models/event';
 import { FilterQuery, Types } from 'mongoose';
-
+import { Redis } from 'ioredis';
+import redisClient from '@src/config/redis';
+import { IUserDocument } from '@src/models/user';
 export class EventService {
+    private redisClient: Redis;
+
+    constructor(redisClient: Redis) {
+        this.redisClient = redisClient;
+    }
     // Type 如果是 comment, details 應該包含 commentText, postId, commentId
     // Type 如果是 like, details 應該包含 contentId, contentType
     // 其他事件不需要特別驗證 details, 但是 get 後 details 會是 {}
@@ -53,8 +60,38 @@ export class EventService {
             details: details,
         };
 
-        await new Event(eventData).save();
+        const newEvent = await new Event(eventData).save();
+        const formattedEvent = this.formatNotification(newEvent);
+        const redisKey = `events:${receiver.toString()}`;
+        await this.redisClient.lpush(redisKey, JSON.stringify(formattedEvent));
+        await this.redisClient.ltrim(redisKey, 0, 99); // 保留最新 100 條事件
+        await this.redisClient.expire(redisKey, 600); // 設置過期時間為 10 分鐘
+
+        return;
+    }
+    private formatNotification(notification: IEventDocument) {
+        return {
+            _id: notification._id.toString(),
+            eventType: notification.eventType,
+            timestamp: notification.timestamp.toISOString(),
+            sender: {
+                _id: (notification.sender as IUserDocument)._id.toString(),
+                accountName: (notification.sender as IUserDocument).accountName,
+                avatarUrl: (notification.sender as IUserDocument).avatarUrl,
+            },
+            receiver: {
+                _id: (notification.receiver as IUserDocument)._id.toString(),
+                accountName: (notification.receiver as IUserDocument).accountName,
+                avatarUrl: (notification.receiver as IUserDocument).avatarUrl,
+            },
+            details: notification.details || {},
+        };
+    }
+
+    // 格式化多個通知
+    private formatNotifications(notifications: IEventDocument[]) {
+        return notifications.map(notification => this.formatNotification(notification));
     }
 }
 
-export const eventService = new EventService();
+export const eventService = new EventService(redisClient);
